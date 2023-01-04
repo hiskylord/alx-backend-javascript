ceefed43dd14a97395e809e66d9ad116155911f7
mac <mac@macs-MacBook-Air.local>
2023-01-04 02:44:03 +0100

    ES6 data Manipulation set

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,14 @@
 export const weakMap = new WeakMap();
-
+/*
+weak array
+*/
 export const queryAPI = (endpoint) => {
   if (weakMap.has(endpoint)) {
     const endpointData = weakMap.get(endpoint);
     if (endpointData >= 4) {
-      throw new Error('Endpoint load is high');
+      throw new Error("Endpoint load is high");
     }
-    weakMap.set(endpoint, (endpointData + 1));
+    weakMap.set(endpoint, endpointData + 1);
   } else {
     weakMap.set(endpoint, 1);
   }
