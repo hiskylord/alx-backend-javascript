ff330915a4530e2223d1bc7b269997c784e8e316
mac <mac@macs-MacBook-Air.local>
2023-01-05 07:02:04 +0100

    typescripts

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 const cleanSet = (set, startString) => {
   const strings = [];
 
-  if (startString === '' || typeof startString !== 'string') return '';
+  if (startString === "" || typeof startString !== "string") return "";
   set.forEach((s) => {
-    if (typeof s === 'string' && s.startsWith(startString)) {
+    if (typeof s === "string" && s.startsWith(startString)) {
       strings.push(s.slice(startString.length));
     }
   });
-  return strings.join('-');
+  return strings.join("-");
 };
 
 export default cleanSet;
